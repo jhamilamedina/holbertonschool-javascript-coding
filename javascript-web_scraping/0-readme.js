a50d2f8107e42a0fb746d14de1ebb0b094aa7430
Maly <jhamilamedina98xd@gmail.com>
2024-04-03 20:11:49 -0500

    feat: Script que lea e imprima el archivo

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+let fs = require('fs');
+
+// File path
+let filePath = 'cisfun';
+
+// Verificar si se promociono un argumentos
+if (!filePath) {
+  console.error('Debes proporcionar la ruta del archivo como argumento.');
+  process.exit(1);
+}
+
+// leer el achivo
+fs.readFile(filePath, 'utf8', (error, data) => {
+  if (error) {
+    console.error('the error object:');
+    return
+  }
+
+  // Mostrar el contenido del archivo
+  console.log(data);
+});
