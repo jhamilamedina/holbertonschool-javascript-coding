e080d4724a299561163309e900bcc69efefda201
Maly <jhamilamedina98xd@gmail.com>
2024-04-06 17:00:30 -0500

    feat: Obtener el contenido de una pagina web y almacenarlo en un archivo

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,19 +6,25 @@ const request = require('request');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
+if (!url || !filePath) {
+  console.log('Ingresa una URL');
+  process.exit(1);
+}
 // Realizar la solicitud GET a la API
 request(url, (error, response, body) => {
   if (error) {
-    console.error('Error al realizar la solicitud:', error);
-    return;
+    console.log(error);
+    process.exit(1);
   }
 
-  fs.writeFile(filePath, body, 'utf-8', (error) => {
-    if (error) {
-      console.error('Error al scribir el archivo:', error);
-      return;
-    }
-
-    console.log(filePath);
-  });
+  // Escribir y almacenar el contenido de la web en un archivo
+  if (response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf-8', function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  } else {
+    console.log(`Error: ${response.statusCode}`);
+  }
 });
