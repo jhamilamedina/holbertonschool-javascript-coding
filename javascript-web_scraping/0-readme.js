a454c7a1b561622af57ccc95cd168dcdf6e7441a
Maly <jhamilamedina98xd@gmail.com>
2024-04-05 19:57:20 -0500

    feat: Script que lee e imprime el contenido del archivo

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
-const fs = require('fs');
 
+const fs = require('fs');
 // File path
 const filePath = process.argv[2];
 
-// leer el achivo
-fs.readFile(filePath, 'utf8', (error, data) => {
-  if (error) {
-    console.error('', error);
-    return;
-  }
+if (!filePath) {
+  console.error('Error: ruta invalida.');
+  process.exit(1);
+}
 
-  // Mostrar el contenido del archivo
-  console.log(data);
+// leer el achivo
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+  }else {
+    console.log(data);
+  }
 });
