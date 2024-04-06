5a77e45e144911582ffbaf5f9e98e5898babc9e7
Maly <jhamilamedina98xd@gmail.com>
2024-04-06 14:49:31 -0500

    Script que escriba una cadena en un archivo

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,24 +1,17 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-// File Patnodh obtener la ruta del archivo
-const filePath = process.argv[2];
+const FILE_PATH = process.argv[2];
+const STRING_TO_WRITE = process.argv[3];
 
-// Obtener la cadena a escribir del 2do argumento
-const contentToWrite = process.argv[3];
-
-// Verificar si se proporcionaron los argumentos necesarios
-if (!filePath || !contentToWrite) {
-  console.error('Debes proporcionar la ruta del archivo.');
-  process.exit(1); // Salir del script con código de error 1
+if (!FILE_PATH) {
+  console.log('Error: proporciona una ruta valida y contenido.');
+  process.exit(1);
 }
 
-// Escribir la cadena en el archivo
-fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
-  if (error) {
-    console.error('Error writing file', error);
-    return;
+fs.writeFile(FILE_PATH, STRING_TO_WRITE, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
   }
-
-  console.log('');
 });
