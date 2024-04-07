cdbf16f99a184059dc593712d22ef42352c362f3
Maly <jhamilamedina98xd@gmail.com>
2024-04-07 15:34:18 -0500

    feat: Script que calcula la cantidad de tareas completadas

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+request.get(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error('Error al realizar la solicitud:', error);
+        return;
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('Error en la respuesta de la API:', response.statusCode);
+        return;
+    }
+
+    const tasks = JSON.parse(body);
+
+    const completedTasksByUser = {};
+
+    tasks.forEach(task => {
+        if (task.completed) {
+            if (completedTasksByUser[task.userId]) {
+                completedTasksByUser[task.userId]++;
+            } else {
+                completedTasksByUser[task.userId] = 1;
+            }
+        }
+    });
+
+    for (const userId in completedTasksByUser) {
+        console.log(`${userId}: ${completedTasksByUser[userId]}`);
+    }
+});
