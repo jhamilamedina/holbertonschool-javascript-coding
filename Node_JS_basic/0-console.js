2cce8382b03884f903595068df9d93d16bacd135
Maly <jhamilamedina@gmail.com>
2024-05-01 19:34:09 -0500

    feat: Define una función que imprima un argumento

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+// Crear una función que imprima en STDOUT el argumento de cadena.
+
+function displayMessage(message) {
+  // Imprimiendo el argumento
+console.log(message);
+}
+// Export la funcion displayMessage para usarla en otros archivos
+module.exports = displayMessage;
