eda962698f3d05c5b3a3cf4cfe3cacc425af68fc
Maly <jhamilamedina@gmail.com>
2024-05-01 22:48:56 -0500

    feat: Programa que puede insertar nombres

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,25 @@
+// Crea un programa que se ejecute a traves de la linea de comando
+
+// Importar el módulo 'readline' para leer la entrada del usuario desde la línea de comandos
+const readline = require('readline');
+
+// Crear una interfaz de lectura para leer la entrada del usuario desde la línea de comandos
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Mostrar un mensaje de bienvenida
+console.log('Welcome to Holberton School, what is your name?');
+
+// Leer la entrada del usuario
+rl.on('line', (input) => {
+  // Mostrar el nombre del usuario
+  console.log(`Your name is: ${input}`);
+  
+  // Mostrar un mensaje de despedida y cerrar el programa
+  console.log('This important software is now closing');
+  rl.close();
+  
+    }
+  );
