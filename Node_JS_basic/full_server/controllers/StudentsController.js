165dc322142cd881eb8277329a7aaa4a77769224
Maly <jhamilamedina@gmail.com>
2024-06-12 00:04:23 -0500

    fix: Se realiza un sevidor HTTPcon express

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,38 +1,33 @@
 const { readDatabase } = require('../utils');
 
-export default class StudentsController {
-  static getAllStudents(req, res) {
-    const database = 'database.csv';
-    readDatabase(database)
-      .then((data) => {
-        let textResponse = 'This is the list of our students\n';
-        for (const field in data) {
-          if (Object.prototype.hasOwnProperty.call(data, field)) {
-            textResponse += `Number of students in ${field}: ${data[field].length}. List:${data[field].join(',')}\n`;
-          }
-        }
-        res.status(200).send(`${textResponse.slice(0, -1)}`);
-      })
-      .catch((err) => {
-        // const textResponse = 'This is the list of our students\n';
-        // res.status(500).send(`${textResponse}${err.message}`);
-        res.status(500).send(`${err.message}`);
-      });
+class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const students = await readDatabase(process.argv[2]);
+      let responseText = 'This is the list of our students\n';
+      for (const [field, names] of Object.entries(students).sort(([a], [b]) => a.localeCompare(b))) {
+        responseText += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+      }
+      res.status(200).send(responseText.trim());
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
   }
 
-  static getAllStudentsByMajor(req, res) {
-    const { major } = req.params;
-    if (major !== 'CS' && major !== 'SWE') {
+  static async getAllStudentsByMajor(req, res) {
+    const { major } = req.params.major;
+    if (!['CS', 'SWE'].includes(major)) {
       res.status(500).send('Major parameter must be CS or SWE');
-    } else {
-      const database = 'database.csv';
-      readDatabase(database)
-        .then((data) => {
-          res.status(200).send(`List:${data[major]}`);
-        })
-        .catch((err) => {
-          res.status(500).send(err.message);
-        });
+      return;
+    }
+    try {
+      const students = await readDatabase(process.argv[2]);
+      const names = students[major] || [];
+      res.status(200).send(`List: ${names.join(', ')}`);
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
     }
   }
 }
+
+module.exports = StudentsController;
