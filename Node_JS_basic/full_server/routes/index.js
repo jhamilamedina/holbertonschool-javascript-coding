2b968859c8794c42de6f23a4a877f04fedc7441c
Maly <jhamilamedina@gmail.com>
2024-06-11 20:22:23 -0500

    fix: Se realiza un sevidor HTTPcon express

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -3,10 +3,11 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-const router = express.Router();
+const app = express();
 
-router.get('/', AppController.getHomepage);
-router.get('/students', StudentsController.getAllStudents);
-router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+app.get('/', AppController.getHomepage);
 
-module.exports = router;
+app.get('/students', StudentsController.getAllStudents);
+app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+export default app;
